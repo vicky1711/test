60f3a0d430ca8323d19768f91f24e9fd87d788fb
vicky1711 <vicky@tasproit.com>
2021-05-05 16:13:57 +0530

    login

---
+++ loginForm/src/demotest/Demo2.java
@@ -0,0 +1,55 @@
+package demotest;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Demo2
+ */
+@WebServlet("/Demo2")
+public class Demo2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private PrintWriter out;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Demo2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		if(username.equalsIgnoreCase("vicky") && password.equals("tasproit")){  
+	        response.sendRedirect("Demo3.jsp");
+			  
+    }  
+		else{
+			response.sendRedirect("Demo4.jsp");
+		}
+		
+	}
+	
+}
