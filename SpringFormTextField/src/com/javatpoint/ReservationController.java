73e6a2b52dff2862e82fefc2bb69e97cdf1bfad3
vicky1711 <vicky@tasproit.com>
2021-05-05 15:58:34 +0530

    second project

---
+++ SpringFormTextField/src/com/javatpoint/ReservationController.java
@@ -0,0 +1,25 @@
+package com.javatpoint;  
+import org.springframework.stereotype.Controller;  
+import org.springframework.ui.Model;  
+import org.springframework.web.bind.annotation.ModelAttribute;  
+import org.springframework.web.bind.annotation.RequestMapping;  
+  
+@RequestMapping("/reservation")  
+@Controller  
+
+public class ReservationController {
+	@RequestMapping("/bookingForm")
+	
+	public String bookingForm(Model model)  {
+		Reservation res= new Reservation();
+		
+		model.addAttribute("reservation", res);  
+		return "reservation-page";
+	}
+	@RequestMapping("/submitForm")  
+	// @ModelAttribute binds form data to the object  
+	public String submitForm(@ModelAttribute("reservation") Reservation res)  
+	{  
+	    return "confirmation-page";  
+	}  
+}
